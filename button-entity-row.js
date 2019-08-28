0d895bda6b5faa81c7a3b2382888201cab963172
Eric Matte <14026234@usherbrooke.ca>
2019-08-28 00:02:55 -0400

    Upgrade button-entity-row
    - Migrate to lit-element
    - Fix edge cases
    - Update README.md
    - Add state icons
    - Add support for more default services
    - Add Prettier to the project
    - Refactor

--- button-entity-row.js
+++ button-entity-row.js
@@ -1,190 +1,178 @@
-class ButtonEntityRow extends Polymer.Element {
-  static get template() {
-    return Polymer.html`
-<style>
- hui-generic-entity-row {
-     margin: var(--ha-themed-slider-margin, initial);
- }
- .flex {
-     display: flex;
-     align-items: center;
- }
- .flex-box {
-     display: flex;
-     justify-content: space-evenly;
- }
- paper-button {
-     color: var(--primary-color);
-     font-weight: 500;
-     cursor: pointer;
-     padding: 8px;
-     position: relative;
-     display: inline-flex;
-     align-items: center;
- }
- .icon-default {
-	 color: var(--primary-color);
- }
- .icon-red {
-	 color: var(--google-red-500);
- }
- .icon-green {
-	 color: var(--google-green-500);
- }
- .icon-yellow {
-	 color: var(--google-yellow-500);
- }
- .icon-grey {
-	 color: var(--paper-grey-200);
- }
-</style>
-<template is="dom-repeat" items="[[buttons]]" as="row">
-    <div class="flex-box">
-        <template is="dom-repeat" items="[[row]]">
-            <paper-button on-click="handleButton" style="[[getStyle(item.style)]]">
-                <template is="dom-if" if="{{item.icon}}">
-                    <ha-icon icon="[[item.icon]]" class$="[[getClass(item.icon_color)]]" style="[[getPadding(item.name)]]"><ha-icon>
-                </template>
-                <template is="dom-else">
-                    <ha-state-icon state="[[item.state]]""><ha-state-icon>
-                </template>
-                {{item.name}}
-                <paper-ripple center class$="[[getRippleStyle(item.name)]]"></paper-ripple>
-            </paper-button>
-        </template>
-</template>
+import { LitElement, html, css } from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module"
 
+class ButtonEntityRow extends LitElement {
+  static get properties() {
+    return {
+      hass: { type: Object },
+      config: { type: Object },
+      buttons: { type: Array }
+    }
+  }
+
+  static get styles() {
+    return css`
+      hui-generic-entity-row {
+        margin: var(--ha-themed-slider-margin, initial);
+      }
+      .flex-box {
+        display: flex;
+        justify-content: space-evenly;
+      }
+      paper-button {
+        cursor: pointer;
+        padding: 8px;
+        position: relative;
+        display: inline-flex;
+        align-items: center;
+      }
+      .button-default {
+        color: var(--paper-item-icon-color); /*var(--primary-color); */
+      }
+      .button-active {
+        color: var(--paper-item-icon-active-color);
+      }
+      .button-inactive {
+        color: var(--paper-item-icon-color);
+      }
     `
   }
 
-  static get properties() {
-    return {
-      _hass: Object,
-      _config: Object,
-      buttons: Array,
-      stateObj: { type: Object, value: null },
-    };
+  render() {
+    return html`
+      <div class="flex-box">
+        ${this.buttons.map(button => {
+          const state = this.hass.states[button.entityId] || {}
+          const icon = this.getCurrentIcon(button, state)
+          const name = button.name || (!icon && state.attributes ? state.attributes.friendly_name : null)
+
+          return html`
+            <paper-button
+              @click="${() => this.handleButtonClick(button)}"
+              style="${this.getStyle(button.style)}"
+              class="${this.getClass(state.state)}"
+            >
+              ${icon &&
+                html`
+                  <ha-icon icon="${icon}" style="${name ? "padding-right: 5px;" : ""}"></ha-icon>
+                `}
+              ${name}
+              <paper-ripple center class="${name ? "" : "circle"}"></paper-ripple>
+            </paper-button>
+          `
+        })}
+      </div>
+    `
   }
 
-  getClass(color) {
-    return color ? `icon-${color}` : '';
+  getCurrentIcon(button, state) {
+    let icon = button.icon
+    if (state.attributes) {
+      if (button.stateIcons && button.stateIcons[state.state]) {
+        icon = button.stateIcons[state.state]
+      }
+      if (!icon) {
+        icon = state.attributes.icon
+      }
+    }
+    return icon
+  }
+
+  getClass(state) {
+    switch (state) {
+      case "on":
+        return "button-active"
+      case "off":
+        return "button-inactive"
+      default:
+        return "button-default"
+    }
+  }
+
+  withDefaultEntityService(entityId) {
+    const domain = entityId.split(".")[0]
+    let service
+    switch (domain) {
+      case "automation":
+      case "cover":
+      case "fan":
+      case "input_boolean":
+      case "light":
+      case "script":
+      case "switch":
+      case "vacuum":
+        service = "toggle"
+        break
+      case "media_player":
+        service = "media_play_pause"
+        break
+      case "scene":
+        service = "turn_on"
+        break
+      default:
+        throw new Error(`service required, but was not found for ${entityId}`)
+    }
+
+    return {
+      service: `${domain}.${service}`,
+      serviceData: {
+        entity_id: entityId
+      }
+    }
   }
 
   getStyle(styles) {
-    var style = "";
-    for (let index in styles) {
-      if (styles.hasOwnProperty(index)) {
-        for (let s in styles[index]) {
-          style += `${s}: ${styles[index][s]};`;
-        }
-      }
-    }
-    return style;
+    return Object.keys(styles || {})
+      .reduce((style, rule) => {
+        return [...style, `${rule}: ${styles[rule]};`]
+      }, [])
+      .join(" ")
   }
 
-  getPadding(name) {
-    return name ? 'padding-right: 5px;' : '';
-  }
-
-  getRippleStyle(name) {
-    return name ? '' : 'circle';
-  }
-
-  makeButtonFromEntity(entity) {
-    const parts = entity.split('.')
-    const domain = parts[0]
-    let service
-    switch (domain) {
-    case 'light':
-    case 'switch':
-    case 'script':
-      service = 'toggle'
-      break
-    case 'media_player':
-      service = 'media_play_pause'
-      break
-    case 'scene':
-      service = 'turn_on'
-      break
-    default:
-      throw new Error(`cannot use ${entity} without a specifig action config`)
-    }
-    return {
-      service: `${domain}.${service}`,
-      service_data: {
-        entity_id: entity
-      }
-    }
-  }
-  
   setConfig(config) {
-    this._config = config;
-    if (!config.buttons) {
-      throw new Error("missing buttons")
-    }
-    if (!Array.isArray(config.buttons)) {
-      throw new Error("buttons must be an array")
-    }
-    if (config.buttons.length <= 0) {
-      throw new Error("at least one button required")
-    }
-    if (!Array.isArray(config.buttons[0])) {
-      config.buttons = [config.buttons]
-    }
-    
-    this.buttons = config.buttons.map(row => row.map(button => {
-      if (typeof button === "string") {
-        return this.makeButtonFromEntity(button)
-      }
-      if (button.entity) {
-        return {
-          ...this.makeButtonFromEntity(button.entity),
-          icon: button.icon,
-          icon_color: button.icon_color,
-          style: button.style,
-          name: button.name
+    if (!config.buttons) throw new Error("missing buttons")
+    if (!Array.isArray(config.buttons)) throw new Error("buttons must be an array")
+    if (config.buttons.length <= 0) throw new Error("at least one button required")
+
+    this.config = config
+    this.buttons = config.buttons.map(item => {
+      let button
+      if (typeof item === "string") {
+        button = {
+          entityId: item
         }
+      } else if (item.entity) {
+        button = {
+          entityId: item.entity,
+          icon: item.icon,
+          stateIcons: item.state_icons,
+          style: item.style,
+          name: item.name,
+          service: item.service,
+          serviceData: item.service_data
+        }
+      } else {
+        throw new Error("button config is not supported")
       }
 
-      if (!button.service) throw new Error("service required")
-      if (!button.service_data) button.service_data = {}
-      if (!button.name && !button.icon) throw new Error("name or icon required")
+      if (!button.service) {
+        button = { ...button, ...this.withDefaultEntityService(button.entityId) }
+      }
+
+      if (Array.isArray(button.serviceData)) {
+        button.serviceData = button.serviceData.reduce((obj, item) => ({ ...obj, ...item }), {})
+      }
+
+      if (button.serviceData === null || typeof button.serviceData !== "object")
+        throw new Error("serviceData must be an object")
+
       return button
-    }))
-
-    
+    })
   }
 
-  set hass(hass) {
-    this._hass = hass;
-
-    this.buttons = this.buttons.map(row => row.map(button => {
-      const state = hass.states[button.service_data.entity_id]
-      if (state) {
-        button.state = state
-      }
-      if (button.name) return button
-      if (button.icon) return button
-
-      if (state && state.attributes && state.attributes.friendly_name) {
-        button.name = state.attributes.friendly_name
-        return button
-      }
-      return button
-    }))
-
-  }
-
-  handleButton(evt) {
-    const button = evt.model.get('item')
-    const svc = button.service.split('.')
-    this._hass.callService(svc[0], svc[1], button.service_data)
-  }
-
-
-  stopPropagation(ev) {
-    ev.stopPropagation();
+  handleButtonClick(button) {
+    const service = button.service.split(".")
+    this.hass.callService(service[0], service[1], button.serviceData)
   }
 }
 
-customElements.define('button-entity-row', ButtonEntityRow);
+customElements.define("button-entity-row", ButtonEntityRow)
